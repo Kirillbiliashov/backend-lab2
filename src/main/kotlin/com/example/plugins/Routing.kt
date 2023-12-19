01edeb7de5082086589ed3e7a966946d9d44ff03
kirillbiliashov <Kirillbeliashov14@gmail.com>
2023-12-19 22:22:57 +0200

    Restricted endpoints access only to uathenticated users

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -29,16 +29,29 @@ private val expensesDao = ExpensesDaoImpl()
 private val usersCredentialsDao = UsersCredentialsDaoImpl()
 
 fun Application.configureRouting() {
+    install(Authentication) {
+        jwt("app-validation") {
+            verifier(JWT
+                .require(Algorithm.HMAC256(System.getenv("secretKey")))
+                .withIssuer("backend-lab4")
+                .build())
+            validate {
+                val username = it.payload.getClaim("username").asString()
+                val password = it.payload.getClaim("password").asString()
+                if (username != null && password != null &&
+                    usersCredentialsDao.exists(UserCredentials(username, password))) {
+                    JWTPrincipal(it.payload)
+                } else {
+                    null
+                }
+            }
+        }
+    }
     routing {
         userRouting()
         categoryRouting()
         recordRouting()
         currencyRouting()
-    }
-    install(Authentication) {
-        jwt {
-            // Configure jwt authentication
-        }
     }
     install(RequestValidation) {
         validate<User> { user ->
@@ -80,25 +93,27 @@ fun Application.configureRouting() {
 }
 
 fun Routing.userRouting() {
-    get("/user/{id}") {
-        val id = call.parameters["id"]!!.toInt()
-        val user = usersDao.get(id)
-        if (user == null) call.respond(HttpStatusCode.NotFound)
-        else call.respond(HttpStatusCode.OK, user)
-    }
+    authenticate("app-validation") {
+        get("/user/{id}") {
+            val id = call.parameters["id"]!!.toInt()
+            val user = usersDao.get(id)
+            if (user == null) call.respond(HttpStatusCode.NotFound)
+            else call.respond(HttpStatusCode.OK, user)
+        }
 
-    delete("/user/{id}") {
-        val id = call.parameters["id"]!!.toInt()
-        usersDao.delete(id)
-        call.respond(HttpStatusCode.NoContent, "user $id successfully deleted")
-    }
-    post("/user") {
-        val user = call.receive<User>()
-        val id = usersDao.insert(user)
-        call.respond(IdBody(id))
-    }
-    get("/users") {
-        call.respond(usersDao.getAll())
+        delete("/user/{id}") {
+            val id = call.parameters["id"]!!.toInt()
+            usersDao.delete(id)
+            call.respond(HttpStatusCode.NoContent, "user $id successfully deleted")
+        }
+        post("/user") {
+            val user = call.receive<User>()
+            val id = usersDao.insert(user)
+            call.respond(IdBody(id))
+        }
+        get("/users") {
+            call.respond(usersDao.getAll())
+        }
     }
 
     post("/user/signup") {
@@ -119,75 +134,80 @@ fun Routing.userRouting() {
 }
 
 fun Routing.categoryRouting() {
-    get("/category/{id}") {
-        val id = call.parameters["id"]!!.toInt()
-        val category = categoriesDao.get(id)
-        if (category == null) call.respond(HttpStatusCode.NotFound)
-        else call.respond(HttpStatusCode.OK, category)
+    authenticate("app-validation") {
+        get("/category/{id}") {
+            val id = call.parameters["id"]!!.toInt()
+            val category = categoriesDao.get(id)
+            if (category == null) call.respond(HttpStatusCode.NotFound)
+            else call.respond(HttpStatusCode.OK, category)
+        }
+        delete("/category/{id}") {
+            val id = call.parameters["id"]!!.toInt()
+            categoriesDao.delete(id)
+            call.respond(HttpStatusCode.NoContent)
+        }
+        post("/category") {
+            val category = call.receive<Category>()
+            val id = categoriesDao.insert(category)
+            call.respond(IdBody(id))
+        }
     }
-    delete("/category/{id}") {
-        val id = call.parameters["id"]!!.toInt()
-        categoriesDao.delete(id)
-        call.respond(HttpStatusCode.NoContent)
-    }
-    post("/category") {
-        val category = call.receive<Category>()
-        val id = categoriesDao.insert(category)
-        call.respond(IdBody(id))
-    }
-
 }
 
 fun Routing.currencyRouting() {
-    get("/currency/{id}") {
-        val id = call.parameters["id"]!!.toInt()
-        val currency = currenciesDao.get(id)
-        if (currency == null) call.respond(HttpStatusCode.NotFound)
-        else call.respond(HttpStatusCode.OK, currency)
-    }
-    delete("/currency/{id}") {
-        val id = call.parameters["id"]!!.toInt()
-        if (id <= 2) call.respond(HttpStatusCode.BadRequest, "Can't delete default currency")
-        else {
-            currenciesDao.delete(id)
-            call.respond(HttpStatusCode.NoContent)
+    authenticate("app-validation") {
+        get("/currency/{id}") {
+            val id = call.parameters["id"]!!.toInt()
+            val currency = currenciesDao.get(id)
+            if (currency == null) call.respond(HttpStatusCode.NotFound)
+            else call.respond(HttpStatusCode.OK, currency)
         }
-    }
-    post("/currency") {
-        val currency = call.receive<Currency>()
-        val id = currenciesDao.insert(currency)
-        call.respond(IdBody(id))
+        delete("/currency/{id}") {
+            val id = call.parameters["id"]!!.toInt()
+            if (id <= 2) call.respond(HttpStatusCode.BadRequest, "Can't delete default currency")
+            else {
+                currenciesDao.delete(id)
+                call.respond(HttpStatusCode.NoContent)
+            }
+        }
+        post("/currency") {
+            val currency = call.receive<Currency>()
+            val id = currenciesDao.insert(currency)
+            call.respond(IdBody(id))
+        }
     }
 
 }
 
 fun Routing.recordRouting() {
-    get("/record/{id}") {
-        val id = call.parameters["id"]!!.toInt()
-        val expense = expensesDao.get(id)
-        if (expense == null) call.respond(HttpStatusCode.NotFound)
-        else call.respond(HttpStatusCode.OK, expense)
-    }
+    authenticate("app-validation") {
+        get("/record/{id}") {
+            val id = call.parameters["id"]!!.toInt()
+            val expense = expensesDao.get(id)
+            if (expense == null) call.respond(HttpStatusCode.NotFound)
+            else call.respond(HttpStatusCode.OK, expense)
+        }
 
-    delete("/record/{id}") {
-        val id = call.parameters["id"]!!.toInt()
-        expensesDao.delete(id)
-        call.respond(HttpStatusCode.NoContent)
-    }
+        delete("/record/{id}") {
+            val id = call.parameters["id"]!!.toInt()
+            expensesDao.delete(id)
+            call.respond(HttpStatusCode.NoContent)
+        }
 
-    post("/record") {
-        val record = call.receive<Expense>()
-        val id = expensesDao.insert(record)
-        call.respond(IdBody(id))
-    }
+        post("/record") {
+            val record = call.receive<Expense>()
+            val id = expensesDao.insert(record)
+            call.respond(IdBody(id))
+        }
 
-    get("/records") {
-        val userId = call.parameters["user_id"]?.toInt()
-        val categoryId = call.parameters["category_id"]?.toInt()
-        if (userId == null && categoryId == null) {
-            call.respond(HttpStatusCode.BadRequest, "At least user id or category id needs to be passed")
-        } else {
-            call.respond(expensesDao.getQuery(userId, categoryId))
+        get("/records") {
+            val userId = call.parameters["user_id"]?.toInt()
+            val categoryId = call.parameters["category_id"]?.toInt()
+            if (userId == null && categoryId == null) {
+                call.respond(HttpStatusCode.BadRequest, "At least user id or category id needs to be passed")
+            } else {
+                call.respond(expensesDao.getQuery(userId, categoryId))
+            }
         }
     }
 
